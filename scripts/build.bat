2abb6ea86784db7c852069350270019e4758c719
honkstar1 <hkarlsson@gmail.com>
2022-05-03 22:56:31 -0700

    Change build script to default to vs2022

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,7 +2,8 @@
 @pushd build
 
 rem @call cmake .. -G "Visual Studio 15 2017 Win64" -DEACOPY_BUILD_TESTS:BOOL=ON
-@call cmake .. -G "Visual Studio 16 2019" -A x64 -DEACOPY_BUILD_TESTS:BOOL=ON
+rem @call cmake .. -G "Visual Studio 16 2019" -A x64 -DEACOPY_BUILD_TESTS:BOOL=ON
+@call cmake .. -G "Visual Studio 17 2022" -DEACOPY_BUILD_TESTS:BOOL=ON
 @call cmake --build . --config Release
 @call cmake --build . --config Debug
 
